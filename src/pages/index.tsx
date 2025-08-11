f4d55bbe8db0ce61c37a7be2cb253e21be07adbe
Lee Goddard <leegee@gmail.com>
2025-08-11 09:43:35 +0200

    Map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,57 @@
-import { createSignal } from 'solid-js';
+import { onMount } from "solid-js";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function Home() {
-  const [count, setCount] = createSignal(0);
+  let mapContainer: HTMLDivElement | undefined;
+
+  onMount(() => {
+    if (!mapContainer) return;
+
+    // const map = new maplibregl.Map({
+    //   container: mapContainer,
+    //   style: "https://demotiles.maplibre.org/style.json",
+    //   center: [0, 0],
+    //   zoom: 2,
+    // });
+
+    const map = new maplibregl.Map({
+      container: mapContainer,
+      style: {
+        version: 8,
+        sources: {
+          cartoDark: {
+            type: "raster",
+            tiles: [
+              "https://basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+              "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+              "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+              "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+              "https://d.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+            ],
+            tileSize: 256,
+            attribution:
+              '© <a href="https://carto.com/attributions">CARTO</a> © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          },
+        },
+        layers: [
+          {
+            id: "cartoDarkLayer",
+            type: "raster",
+            source: "cartoDark",
+            minzoom: 0,
+            maxzoom: 19,
+          },
+        ],
+      },
+      center: [0, 0],
+      zoom: 2,
+    });
+
+    return () => map.remove();
+  });
 
   return (
-    <section class="bg-gray-100 text-gray-700 p-8">
-      <h1 class="text-2xl font-bold">Home</h1>
-      <p class="mt-4">This is the home page.</p>
-
-      <div class="flex items-center space-x-2">
-        <button
-          type="button"
-          class="border rounded-lg px-2 border-gray-900"
-          onClick={() => setCount(count() - 1)}
-        >
-          -
-        </button>
-
-        <output class="p-10px">Count: {count()}</output>
-
-        <button
-          type="button"
-          class="border rounded-lg px-2 border-gray-900"
-          onClick={() => setCount(count() + 1)}
-        >
-          +
-        </button>
-      </div>
-    </section>
+    <div style={{ width: "100vw", height: "100vh" }} ref={mapContainer}></div>
   );
 }
